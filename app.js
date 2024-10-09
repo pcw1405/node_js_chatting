cdb43f9257f1dd80b6943e5082edd1d4e3b41012
chungwon <pcw14057@gmail.com>
2024-10-09 17:53:37 +0900

    데이터베이스 연결

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
+require('dotenv').config() // 이게있어야  process
+const cors =require("cors")
 const app= express()
+app.use(cors())
+// 테스트를 위해서 
 
- mongoose.connect("데이터베이스 주소",{
-    userNewUrlParser : true,
-    useUnifiedTopology: true,
+ mongoose
+    .connect(process.env.DB,{ //DB를 읽어오란 뜻
+        userNewUrlParser : true,
+        useUnifiedTopology: true,
 
- }).then(()=>console.log("connected to database"));
+    }).then(()=>console.log("connected to database"));
 
  module.exports=app

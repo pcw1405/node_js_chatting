2ec691ada9d946965c58f76745aa51c2cdae9dc4
chungwon <pcw14057@gmail.com>
2024-10-15 17:47:38 +0900

    연결 끊는 테스트

--- utils/io.js
+++ utils/io.js
@@ -2,6 +2,13 @@ module.exports=function(io){
 
     io.on("connection",async(socket)=>{
         console.log("client is connected",socket.id);
+
+
+
+        socket.on("disconnect", ()=>{
+            console.log("user is disconnectd");
+        });
+
     });
     //io관련 모든일을 여기서 한다 
     // 연결된 사람을 소켓이라고 한다 //  소켓정보로 매개변수로 받아준다

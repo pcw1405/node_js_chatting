99671603d281fa2f2cb2bf83c39020ee8f3b31bb
chungwon <pcw14057@gmail.com>
2024-10-16 00:25:31 +0900

    프론트엔드 추가 및 백엔드 만드는중

--- node_js_chatting/utils/io.js
+++ node_js_chatting/utils/io.js
@@ -3,7 +3,9 @@ module.exports=function(io){
     io.on("connection",async(socket)=>{
         console.log("client is connected",socket.id);
 
-
+        socket.on("login",async(userName,cb)=>{
+            console.log("backend",userName);
+        })
 
         socket.on("disconnect", ()=>{
             console.log("user is disconnectd");
